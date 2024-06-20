23dd0a6de53ff35dc732f3497987a091b574e19c
Florian Bartels <Florian.Bartels@elektrobit.com>
2024-06-20 09:34:22 +0200

    cleanup, build sysroot/image

--- test-zenoh.sh
+++ test-zenoh.sh
@@ -2,19 +2,11 @@
 
 source /build/scripts/env.sh
 
-# todo: move to Dockerfile
-rustup target add aarch64-unknown-linux-gnu
-
 echo '
 [target.aarch64-unknown-linux-gnu]
 linker = "/usr/bin/aarch64-linux-gnu-gcc-11"
 rustflags = ["-C", "link-args=--sysroot /build/sysroot_aarch64/"]
 ' >> ~/.cargo/config.toml
 
-ls -lha /
-whoami
-
 cd /src
-# todo: properly install toolchain first
-rm rust-toolchain.toml
 cargo b --target aarch64-unknown-linux-gnu

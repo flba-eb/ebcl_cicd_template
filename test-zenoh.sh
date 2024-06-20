f03eeeb49b6177e658ebe93a258d01692364520c
Florian Bartels <Florian.Bartels@elektrobit.com>
2024-06-20 08:55:48 +0200

    Run inside container

--- test-zenoh.sh
+++ test-zenoh.sh
@@ -11,7 +11,7 @@ linker = "/usr/bin/aarch64-linux-gnu-gcc-11"
 rustflags = ["-C", "link-args=--sysroot /build/sysroot_aarch64/"]
 ' >> ~/.cargo/config.toml
 
-cd zenoh
+cd /src
 # todo: properly install toolchain first
 rm rust-toolchain.toml
 cargo b --target aarch64-unknown-linux-gnu

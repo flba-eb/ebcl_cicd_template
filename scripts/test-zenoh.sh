55108d398dade34c9f490e7797af833231c18203
Florian Bartels <Florian.Bartels@elektrobit.com>
2024-06-21 11:32:19 +0200

    Install Rust 1.75.0

--- scripts/test-zenoh.sh
+++ scripts/test-zenoh.sh
@@ -34,5 +34,7 @@ if [[ "$*" == *"--only-one-test"* ]] ; then
     cd commons/zenoh-buffers
 fi
 
+rustup install 1.75.0
+rustup target add aarch64-unknown-linux-gnu --toolchain 1.75.0
 cargo t --target aarch64-unknown-linux-gnu --no-run
 cargo t --target aarch64-unknown-linux-gnu -j1
